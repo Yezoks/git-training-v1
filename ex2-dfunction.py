687bc2fb18ed80fc848747223fbbccd7b51513b4
adeyemi <yezoksworld@gmail.com>
2024-06-12 02:19:39 +0100

    2nd commit: modified ex1 and ex2 python files

---
+++ ex2-dfunction.py
@@ -0,0 +1,9 @@
+def double(n):
+    return 2 * n        #Return twice the given number
+#Call the function with the value of your choice and print its result
+print("To Call this function using its name and assign the value of your. You can run the Python interpreter in the direcotry where you use and then import the module by its name For ex: imprt simple_function and the ncall the function inside module like simple_function1. you can also assign the value obtained from this unction to a number of our choice for ex: x=simple_function1.double(5), and the nprint")
+#x = double(3)
+#print(x)
+print("The program ran successfully")
+#---------------------------------------------------------
+print("first modification for 2nd commit")
